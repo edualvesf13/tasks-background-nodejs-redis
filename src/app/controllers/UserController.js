47803bc55db00a002924fcf3d1ea88514cd0c2db
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-09-08 20:55:13 -0300

    Config sendMail.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,22 @@
 import passwordGenerator from 'password-generator'
+import Mail from '../lib/Mail'
+
 export default {
     async store(req, res){
-        const {nome, email} = req.body
+        const {name, email} = req.body
 
         const user = {
-            nome,
+            name,
             email,
             password: passwordGenerator(15, false)
         }
+
+        await Mail.sendMail({
+            from: 'DIO <contato@dio.com.br>',
+            to: `${name} <${email}>`,
+            subject: 'Cadastro de Usuário',
+            html: `Olá ${name}, bem vindo a DIO! Sua senha é: <strong> ${user.password} </strong>.`
+        })
 
         return res.json(user)
     }
