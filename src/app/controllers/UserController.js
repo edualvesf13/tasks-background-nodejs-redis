540423b65e8beb36c9988b1ede1d09db7ec68815
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-09-08 22:08:40 -0300

    Add bull for manager queues.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import passwordGenerator from 'password-generator'
-import Mail from '../lib/Mail'
+import Queue from '../lib/Queue'
+// import Mail from '../lib/Mail'
 
 export default {
     async store(req, res){
@@ -11,12 +12,16 @@ export default {
             password: passwordGenerator(15, false)
         }
 
-        await Mail.sendMail({
+    /*    await Mail.sendMail({
             from: 'DIO <contato@dio.com.br>',
             to: `${name} <${email}>`,
             subject: 'Cadastro de Usuário',
             html: `Olá ${name}, bem vindo a DIO! Sua senha é: <strong> ${user.password} </strong>.`
         })
+
+    */
+
+        await Queue.add('RegistrationMail', { user })
 
         return res.json(user)
     }
